4886c6c35c19b98c7b67291b87a7c56ba4bece82
aozelce <aozelce@madisoncollege.edu>
2026-05-09 14:04:30 +0300

    Replaced manual methods and constructors in Media entity with Lombok annotations for simplicity.

--- src/main/java/com/aozelce/entity/Media.java
+++ src/main/java/com/aozelce/entity/Media.java
@@ -1,8 +1,7 @@
 package com.aozelce.entity;
 
 import jakarta.persistence.*;
-
-import java.util.Objects;
+import lombok.*;
 
 /**
  * The type Media.
@@ -11,11 +10,18 @@ import java.util.Objects;
  */
 @Entity
 @Table(name = "media")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Media {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @EqualsAndHashCode.Include
     private int id;
 
     @Column(name = "tmdb_id")
@@ -38,204 +44,4 @@ public class Media {
 
     @Column(name = "genres")
     private String genres;
-
-
-    /**
-     * Instantiates a new Media.
-     */
-    public Media() {
-    }
-
-    /**
-     * Instantiates a new Media.
-     *
-     * @param id         the id
-     * @param tmdbId     the tmdb id
-     * @param title      the title
-     * @param mediaType  the media type
-     * @param year       the year
-     * @param posterPath the poster path
-     * @param overview   the overview
-     * @param genres     the genres
-     */
-    public Media(int id, int tmdbId, String title, String mediaType, int year, String posterPath, String overview, String genres) {
-        this.id = id;
-        this.tmdbId = tmdbId;
-        this.title = title;
-        this.mediaType = mediaType;
-        this.year = year;
-        this.posterPath = posterPath;
-        this.overview = overview;
-        this.genres = genres;
-    }
-
-    /**
-     * Gets id.
-     *
-     * @return the id
-     */
-    public int getId() {
-        return id;
-    }
-
-    /**
-     * Sets id.
-     *
-     * @param id the id
-     */
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * Gets tmdb id.
-     *
-     * @return the tmdb id
-     */
-    public int getTmdbId() {
-        return tmdbId;
-    }
-
-    /**
-     * Sets tmdb id.
-     *
-     * @param tmdbId the tmdb id
-     */
-    public void setTmdbId(int tmdbId) {
-        this.tmdbId = tmdbId;
-    }
-
-    /**
-     * Gets title.
-     *
-     * @return the title
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * Sets title.
-     *
-     * @param title the title
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    /**
-     * Gets media type.
-     *
-     * @return the media type
-     */
-    public String getMediaType() {
-        return mediaType;
-    }
-
-    /**
-     * Sets media type.
-     *
-     * @param mediaType the media type
-     */
-    public void setMediaType(String mediaType) {
-        this.mediaType = mediaType;
-    }
-
-    /**
-     * Gets year.
-     *
-     * @return the year
-     */
-    public int getYear() {
-        return year;
-    }
-
-    /**
-     * Sets year.
-     *
-     * @param year the year
-     */
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    /**
-     * Gets poster path.
-     *
-     * @return the poster path
-     */
-    public String getPosterPath() {
-        return posterPath;
-    }
-
-    /**
-     * Sets poster path.
-     *
-     * @param posterPath the poster path
-     */
-    public void setPosterPath(String posterPath) {
-        this.posterPath = posterPath;
-    }
-
-    /**
-     * Gets overview.
-     *
-     * @return the overview
-     */
-    public String getOverview() {
-        return overview;
-    }
-
-    /**
-     * Sets overview.
-     *
-     * @param overview the overview
-     */
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    /**
-     * Gets genres.
-     *
-     * @return the genres
-     */
-    public String getGenres() {
-        return genres;
-    }
-
-    /**
-     * Sets genres.
-     *
-     * @param genres the genres
-     */
-    public void setGenres(String genres) {
-        this.genres = genres;
-    }
-
-    @Override
-    public String toString() {
-        return "Media{" +
-                "id=" + id +
-                ", tmdbId=" + tmdbId +
-                ", title='" + title + '\'' +
-                ", mediaType='" + mediaType + '\'' +
-                ", year=" + year +
-                ", posterPath='" + posterPath + '\'' +
-                ", overview='" + overview + '\'' +
-                ", genres='" + genres + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Media media = (Media) o;
-        return getId() == media.getId() && getTmdbId() == media.getTmdbId() && getYear() == media.getYear() && Objects.equals(getTitle(), media.getTitle()) && Objects.equals(getMediaType(), media.getMediaType()) && Objects.equals(getPosterPath(), media.getPosterPath()) && Objects.equals(getOverview(), media.getOverview()) && Objects.equals(getGenres(), media.getGenres());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getTmdbId(), getTitle(), getMediaType(), getYear(), getPosterPath(), getOverview(), getGenres());
-    }
 }
