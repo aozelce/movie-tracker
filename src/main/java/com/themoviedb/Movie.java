71f742c057585e98d924b2765ff079d14d194c6c
aozelce <aozelce@madisoncollege.edu>
2026-05-09 14:17:42 +0300

    Implemented Lombok in Movie and ResultsItem entities

--- src/main/java/com/themoviedb/Movie.java
+++ src/main/java/com/themoviedb/Movie.java
@@ -1,12 +1,18 @@
 package com.themoviedb;
 
-import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import java.util.List;
 
 /**
  * The type Movie.
  */
-public class Movie{
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Movie {
 
 	@JsonProperty("page")
 	private int page;
@@ -19,87 +25,4 @@ public class Movie{
 
 	@JsonProperty("total_results")
 	private int totalResults;
-
-	/**
-	 * Set page.
-	 *
-	 * @param page the page
-	 */
-	public void setPage(int page){
-		this.page = page;
-	}
-
-	/**
-	 * Get page int.
-	 *
-	 * @return the int
-	 */
-	public int getPage(){
-		return page;
-	}
-
-	/**
-	 * Set total pages.
-	 *
-	 * @param totalPages the total pages
-	 */
-	public void setTotalPages(int totalPages){
-		this.totalPages = totalPages;
-	}
-
-	/**
-	 * Get total pages int.
-	 *
-	 * @return the int
-	 */
-	public int getTotalPages(){
-		return totalPages;
-	}
-
-	/**
-	 * Set results.
-	 *
-	 * @param results the results
-	 */
-	public void setResults(List<ResultsItem> results){
-		this.results = results;
-	}
-
-	/**
-	 * Get results list.
-	 *
-	 * @return the list
-	 */
-	public List<ResultsItem> getResults(){
-		return results;
-	}
-
-	/**
-	 * Set total results.
-	 *
-	 * @param totalResults the total results
-	 */
-	public void setTotalResults(int totalResults){
-		this.totalResults = totalResults;
-	}
-
-	/**
-	 * Get total results int.
-	 *
-	 * @return the int
-	 */
-	public int getTotalResults(){
-		return totalResults;
-	}
-
-	@Override
- 	public String toString(){
-		return 
-			"Movie{" + 
-			"page = '" + page + '\'' + 
-			",total_pages = '" + totalPages + '\'' + 
-			",results = '" + results + '\'' + 
-			",total_results = '" + totalResults + '\'' + 
-			"}";
-		}
 }
